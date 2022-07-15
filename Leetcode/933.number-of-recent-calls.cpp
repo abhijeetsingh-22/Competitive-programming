b8636f3d8ac047e0b4b98c240c82b02cb670b8d6
Abhijeet Singh <asingh9829@gmail.com>
2022-07-15 13:04:35 +0530

    15/7/22 -1

---
+++ Leetcode/933.number-of-recent-calls.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=933 lang=cpp
+ *
+ * [933] Number of Recent Calls
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class RecentCounter
+{
+private:
+    queue<int> q;
+
+public:
+    RecentCounter()
+    {
+    }
+
+    int ping(int t)
+    {
+        q.push(t);
+        while (!q.empty() and q.front() < (t - 3000))
+        {
+            q.pop();
+        }
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
+// @lc code=end
