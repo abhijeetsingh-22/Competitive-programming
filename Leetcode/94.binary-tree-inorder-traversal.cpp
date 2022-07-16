a5611ec47b400ced66ab66f34629aa13487b7537
Abhijeet Singh <asingh9829@gmail.com>
2022-07-17 03:17:32 +0530

    16/7/22 -6 iterative inorder

--- Leetcode/94.binary-tree-inorder-traversal.cpp
+++ Leetcode/94.binary-tree-inorder-traversal.cpp
@@ -21,30 +21,21 @@ class Solution
     /*
     This is TC->O(n) and SC->O(n)
     */
-    // private:
-    //     static void inorder_traversal(TreeNode *root, vector<int> &ans)
-    //     {
-    //         if (root == NULL)
-    //             return;
-    //         inorder_traversal(root->left, ans);
-    //         ans.push_back(root->val);
-    //         inorder_traversal(root->right, ans);
-    //     }
-
-    // public:
-    //     vector<int> inorderTraversal(TreeNode *root)
-    //     {
-    //         vector<int> ans;
-    //         inorder_traversal(root, ans);
-    //         return ans;
-    //     }
+private:
+    static void inorder_rec(TreeNode *root, vector<int> &ans)
+    {
+        if (root == NULL)
+            return;
+        inorder_rec(root->left, ans);
+        ans.push_back(root->val);
+        inorder_rec(root->right, ans);
+    }
 
     // Using morris traversal which uses O(1) space;
-
-public:
-    vector<int> inorderTraversal(TreeNode *root)
+    static void inorder_morris(TreeNode *root, vector<int> &ans)
     {
-        vector<int> ans;
+        if (root == NULL)
+            return;
         TreeNode *cur = root;
         while (cur != NULL)
         {
@@ -55,11 +46,13 @@ public:
                 {
                     prev = prev->right;
                 }
+                // left subtree of cur node has not been visited
                 if (prev->right == NULL)
                 {
                     prev->right = cur;
                     cur = cur->left;
                 }
+                // left subtree have been visited and cur have come 2nd time
                 if (prev->right == cur)
                 {
                     prev->right = NULL;
@@ -68,12 +61,45 @@ public:
                 }
             }
             else
+            // cur have no left subtree or left subtree is null
             {
                 ans.push_back(cur->val);
                 cur = cur->right;
             }
         }
+    }
+    static void inorder_iter(TreeNode *root, vector<int> &ans)
+    {
+        if (root == NULL)
+            return;
+        stack<TreeNode *> stk;
+        TreeNode *ptr = root->left;
+        stk.push(root);
+        while (!stk.empty() or ptr != NULL)
+        {
 
+            if (ptr)
+            {
+                stk.push(ptr);
+                ptr = ptr->left;
+            }
+            else
+            {
+                auto top = stk.top();
+                ptr = top->right;
+
+                ans.push_back(top->val);
+                stk.pop();
+            }
+        }
+    }
+
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        // inorder_morris(root, ans);
+        inorder_iter(root, ans);
         return ans;
     }
 };
