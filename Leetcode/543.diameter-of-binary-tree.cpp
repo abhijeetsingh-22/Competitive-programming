effeaa38e5e28799c387927ed81762f087411e0d
Abhijeet Singh <asingh9829@gmail.com>
2022-07-18 02:07:20 +0530

    17/7/22 -10 iterative

--- Leetcode/543.diameter-of-binary-tree.cpp
+++ Leetcode/543.diameter-of-binary-tree.cpp
@@ -32,11 +32,44 @@ private:
         int diameter = max(left.second + right.second, max(left.first, right.first));
         return {diameter, height};
     }
+    // DFS using stack
+    static int diameter_iter_DFS(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+        unordered_map<TreeNode *, int> height;
+        stack<TreeNode *> stk;
+        stk.push(root);
+        int diameter = 0;
+        while (!stk.empty())
+        {
+            auto top = stk.top();
+            // go to extreme left leaf node
+            if (top->left and height.find(top->left) == height.end())
+            {
+                stk.push(top->left);
+            }
+            else if (top->right and height.find(top->right) == height.end())
+            {
+                stk.push(top->right);
+            }
+            else
+            {
+                stk.pop();
+                int l = height[top->left];
+                int r = height[top->right];
+                height[top] = max(l, r) + 1;
+                diameter = max(diameter, l + r);
+            }
+        }
+        return diameter;
+    }
 
 public:
     int diameterOfBinaryTree(TreeNode *root)
     {
-        return diameter_rec(root).first;
+        // return diameter_rec(root).first;
+        return diameter_iter_DFS(root);
     }
 };
 // @lc code=end
