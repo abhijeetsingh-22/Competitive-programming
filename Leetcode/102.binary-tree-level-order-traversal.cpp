6573a360c6e51afbf39d7d62700fd5020164f542
Abhijeet Singh <asingh9829@gmail.com>
2022-07-17 12:47:38 +0530

    17/7/22 -1

---
+++ Leetcode/102.binary-tree-level-order-traversal.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=102 lang=cpp
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+private:
+    static int height(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+        int l_height = height(root->left);
+        int r_height = height(root->right);
+        return max(l_height, r_height) + 1;
+    }
+    static void print_klevel(TreeNode *root, vector<int> &row, int k)
+    {
+        if (root == NULL)
+            return;
+        if (k == 1)
+        {
+            row.push_back(root->val);
+            return;
+        }
+        print_klevel(root->left, row, k - 1);
+        print_klevel(root->right, row, k - 1);
+    }
+    static void levelorder_rec(TreeNode *root, vector<vector<int>> &ans)
+    {
+        if (root == NULL)
+            return;
+        int h = height(root);
+        for (int i = 1; i <= h; i++)
+        {
+            vector<int> row;
+            print_klevel(root, row, i);
+            ans.push_back(row);
+        }
+    }
+
+public:
+    vector<vector<int>> levelOrder(TreeNode *root)
+    {
+        vector<vector<int>> ans;
+        levelorder_rec(root, ans);
+        return ans;
+    }
+};
+// @lc code=end
