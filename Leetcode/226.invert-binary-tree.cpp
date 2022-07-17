5438690831ff1200c7bf82fc571c533f936f783a
Abhijeet Singh <asingh9829@gmail.com>
2022-07-18 03:45:32 +0530

    17/7/22 -12 iterative

--- Leetcode/226.invert-binary-tree.cpp
+++ Leetcode/226.invert-binary-tree.cpp
@@ -33,11 +33,58 @@ private:
         root->left = root->right;
         root->right = temp;
     }
+    static void invert_iter(TreeNode *root)
+    {
+        if (root == NULL)
+            return;
+        stack<TreeNode *> stk;
+
+        // stk.push(root);
+        TreeNode *cur = root;
+        while (!stk.empty() or cur != NULL)
+        {
+            if (cur)
+            {
+                stk.push(cur);
+                cur = cur->left;
+            }
+            else
+            {
+                TreeNode *temp = stk.top()->right;
+                if (temp == NULL)
+                {
+                    // swap the links
+                    TreeNode *helper;
+                    temp = stk.top();
+
+                    helper = temp->left;
+                    temp->left = temp->right;
+                    temp->right = helper;
+                    stk.pop();
+
+                    while (!stk.empty() and stk.top()->right == temp)
+                    {
+
+                        temp = stk.top();
+                        helper = temp->left;
+                        temp->left = temp->right;
+                        temp->right = helper;
+                        stk.pop();
+                    }
+                }
+                else
+                {
+                    cur = temp;
+                }
+            }
+        }
+    }
 
 public:
     TreeNode *invertTree(TreeNode *root)
     {
-        invert_rec(root);
+        // invert_rec(root);
+        invert_iter(root);
         return root;
     }
 };
