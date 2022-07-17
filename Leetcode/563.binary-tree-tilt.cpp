2c89266d5d939ded7d0f8ec8214e26118086669f
Abhijeet Singh <asingh9829@gmail.com>
2022-07-18 03:03:53 +0530

    17/7/22 -11 iterative

--- Leetcode/563.binary-tree-tilt.cpp
+++ Leetcode/563.binary-tree-tilt.cpp
@@ -31,11 +31,46 @@ private:
         int tsum = abs(l.first - r.first) + l.second + r.second;
         return {sum, tsum};
     }
+    static int tilt_iter(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+        int tsum = 0;
+        stack<TreeNode *> stk;
+        // sum, tsum
+        unordered_map<TreeNode *, int> sum;
+        stk.push(root);
+
+        while (!stk.empty())
+        {
+            TreeNode *top = stk.top();
+            if (top->left and sum.find(top->left) == sum.end())
+            {
+                stk.push(top->left);
+            }
+            else if (top->right and sum.find(top->right) == sum.end())
+            {
+                stk.push(top->right);
+            }
+            else
+            {
+                stk.pop();
+                int l = sum[top->left];
+                int r = sum[top->right];
+                int t = abs(l - r);
+                int s = l + r + top->val;
+                sum[top] = s;
+                tsum += t;
+            }
+        }
+        return tsum;
+    }
 
 public:
     int findTilt(TreeNode *root)
     {
-        return tilt_rec(root).second;
+        // return tilt_rec(root).second;
+        return tilt_iter(root);
     }
 };
 // @lc code=end
