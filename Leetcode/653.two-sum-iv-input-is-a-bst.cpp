95bfd999bb43dd5c35030662f86708683e45c0dd
Abhijeet Singh <asingh9829@gmail.com>
2022-07-19 03:22:10 +0530

    18/7/22 -2 using bst iterator

--- Leetcode/653.two-sum-iv-input-is-a-bst.cpp
+++ Leetcode/653.two-sum-iv-input-is-a-bst.cpp
@@ -16,10 +16,49 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BST_iterator
+{
+private:
+    stack<TreeNode *> stk;
+    bool forward;
+
+public:
+    BST_iterator(TreeNode *root, bool forward)
+    {
+        this->forward = forward;
+        TreeNode *cur = root;
+        while (cur != NULL)
+        {
+            if (cur)
+            {
+                stk.push(cur);
+                cur = forward ? cur->left : cur->right;
+            }
+        }
+    }
+    bool has_next()
+    {
+        return !stk.empty();
+    }
+    int next()
+    {
+        int next_value = stk.top()->val;
+        TreeNode *cur = stk.top();
+        stk.pop();
+        cur = forward ? cur->right : cur->left;
+        while (cur)
+        {
+            stk.push(cur);
+            cur = forward ? cur->left : cur->right;
+        }
+
+        return next_value;
+    }
+};
 class Solution
 {
 private:
-    // TC->O(n) SC->O(2n);
+    // TC->O(n) SC->O(n*h);
     static bool dfs(TreeNode *root, unordered_set<int> &s, int k)
     {
         if (root == NULL)
@@ -29,12 +68,29 @@ private:
         s.insert(root->val);
         return dfs(root->left, s, k) or dfs(root->right, s, k);
     }
+    static bool find_using_bfs_iterator(TreeNode *root, int k)
+    {
+        BST_iterator l_iter(root, true), r_iter(root, false);
+        int left = l_iter.next(), right = r_iter.next();
+        while (left < right)
+        {
+            int sum = left + right;
+            if (sum == k)
+                return true;
+            if (sum < k)
+                left = l_iter.next();
+            else
+                right = r_iter.next();
+        }
+        return false;
+    }
 
 public:
     bool findTarget(TreeNode *root, int k)
     {
-        unordered_set<int> s;
-        return dfs(root, s, k);
+        // unordered_set<int> s;
+        // return dfs(root, s, k);
+        return find_using_bfs_iterator(root, k);
     }
 };
 // @lc code=end
