05f50c9a34ed88cb8d0891e472d1efd7a3048801
Abhijeet Singh <asingh9829@gmail.com>
2022-07-19 23:13:09 +0530

    19/7/22 -4 iterative approach

--- Leetcode/701.insert-into-a-binary-search-tree.cpp
+++ Leetcode/701.insert-into-a-binary-search-tree.cpp
@@ -31,11 +31,34 @@ private:
         root->right = bst_insert(root->right, val);
         return root;
     }
+    static TreeNode *bst_insert_iterative(TreeNode *root, int val)
+    {
+        if (root == NULL)
+            return new TreeNode(val);
+
+        TreeNode *cur = root, *prev;
+        while (cur != NULL)
+        {
+            prev = cur;
+            if (cur->val < val)
+            {
+                cur = cur->right;
+            }
+            else
+                cur = cur->left;
+        }
+        if (prev->val < val)
+            prev->right = new TreeNode(val);
+        else
+            prev->left = new TreeNode(val);
+        return root;
+    }
 
 public:
     TreeNode *insertIntoBST(TreeNode *root, int val)
     {
-        return bst_insert(root, val);
+        // return bst_insert(root, val);
+        return bst_insert_iterative(root, val);
     }
 };
 // @lc code=end
