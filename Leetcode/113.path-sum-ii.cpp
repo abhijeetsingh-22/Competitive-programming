535c7a65ce4f9b82ae0da9738a4bea531c832c91
Abhijeet Singh <asingh9829@gmail.com>
2022-07-20 13:32:40 +0530

    20/7/22 -2 optimized by making vector pass by reference

--- Leetcode/113.path-sum-ii.cpp
+++ Leetcode/113.path-sum-ii.cpp
@@ -19,27 +19,30 @@
 class Solution
 {
 private:
-    static void dfs_path(TreeNode *root, int targetSum, int cur_sum, vector<int> cur_path, vector<vector<int>> &ans)
+    static void dfs_path(TreeNode *root, int targetSum, int cur_sum, vector<int> &cur_path, vector<vector<int>> &ans)
     {
         if (root == NULL)
             return;
-        cur_sum += root->val;
         cur_path.push_back(root->val);
         if (root->left == NULL and root->right == NULL)
         {
-            if (cur_sum == targetSum)
+            if (cur_sum + root->val == targetSum)
                 ans.push_back(cur_path);
+            cur_path.pop_back();
             return;
         }
+        cur_sum += root->val;
         dfs_path(root->left, targetSum, cur_sum, cur_path, ans);
         dfs_path(root->right, targetSum, cur_sum, cur_path, ans);
+        cur_path.pop_back();
     }
 
 public:
     vector<vector<int>> pathSum(TreeNode *root, int targetSum)
     {
         vector<vector<int>> ans;
-        dfs_path(root, targetSum, 0, {}, ans);
+        vector<int> cur_path;
+        dfs_path(root, targetSum, 0, cur_path, ans);
         return ans;
     }
 };
