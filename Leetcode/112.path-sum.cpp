46381c5493701446c5c9a1f1afe62a541445b928
Abhijeet Singh <asingh9829@gmail.com>
2022-07-20 13:15:30 +0530

    20/7/22 -1 iterative

--- Leetcode/112.path-sum.cpp
+++ Leetcode/112.path-sum.cpp
@@ -33,11 +33,39 @@ private:
         }
         return dfs_path_sum(root->left, targetSum, cur_sum) or dfs_path_sum(root->right, targetSum, cur_sum);
     }
+    static bool dfs_path_sum_rec(TreeNode *root, int targetSum)
+    {
+        if (root == NULL)
+            return false;
+
+        stack<pair<TreeNode *, int>> stk;
+        stk.push({root, root->val});
+        while (!stk.empty())
+        {
+            auto top = stk.top();
+            stk.pop();
+            if (top.first->left == NULL and top.first->right == NULL)
+            {
+                if (top.second == targetSum)
+                    return true;
+            }
+            else
+            {
+                if (top.first->left)
+                    stk.push({top.first->left, top.second + top.first->left->val});
+
+                if (top.first->right)
+                    stk.push({top.first->right, top.second + top.first->right->val});
+            }
+        }
+        return false;
+    }
 
 public:
     bool hasPathSum(TreeNode *root, int targetSum)
     {
-        return dfs_path_sum(root, targetSum, 0);
+        // return dfs_path_sum(root, targetSum, 0);
+        return dfs_path_sum_rec(root, targetSum);
     }
 };
 // @lc code=end
