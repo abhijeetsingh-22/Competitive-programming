b9afa25d0bda5832e8be139fd81891696af830be
Abhijeet Singh <asingh9829@gmail.com>
2022-08-05 18:35:46 +0530

    5/8/22 -1 bottom up dp

--- Leetcode/377.combination-sum-iv.cpp
+++ Leetcode/377.combination-sum-iv.cpp
@@ -7,7 +7,7 @@
 // @lc code=start
 class Solution
 {
-    int dp[1001];
+    unsigned int dp[1001];
 
 private:
     int combination_sum(vector<int> &nums, int cur, int target)
@@ -26,12 +26,31 @@ private:
         dp[cur] = ans;
         return ans;
     }
+    int combination_sum_BU(vector<int> &nums, int target)
+    {
+
+        dp[0] = 1;
+        sort(nums.begin(), nums.end());
+        for (int cur_target = 1; cur_target <= target; cur_target++)
+        {
+            dp[cur_target] = 0;
+            for (int &x : nums)
+            {
+                if (x <= cur_target)
+                    dp[cur_target] += dp[cur_target - x];
+                else
+                    break;
+            }
+        }
+        return dp[target];
+    }
 
 public:
     int combinationSum4(vector<int> &nums, int target)
     {
         memset(dp, -1, sizeof(dp));
-        return combination_sum(nums, 0, target);
+        // return combination_sum(nums, 0, target);
+        return combination_sum_BU(nums, target);
     }
 };
 // @lc code=end
