c2612fee9d30d5368621f751eb2a867d63e1f926
Abhijeet Singh <asingh9829@gmail.com>
2022-08-07 13:12:45 +0530

    7/8/22 -1 code cleaning

--- Leetcode/1220.count-vowels-permutation.cpp
+++ Leetcode/1220.count-vowels-permutation.cpp
@@ -8,10 +8,11 @@
 #define mod 1000000007
 class Solution
 {
-    unordered_map<char, int> map;
+    // unordered_map<char, int> map;
+    vector<vector<int>> mappings{{1}, {0, 2}, {0, 1, 3, 4}, {2, 4}, {0}, {0, 1, 2, 3, 4}};
 
 private:
-    int count_vovel_per(int n, vector<vector<long>> &dp, char prev = ' ')
+    int count_vovel_per(int n, vector<vector<long>> &dp, int prev = 5)
     {
         if (n == 0)
             return 1;
@@ -19,40 +20,40 @@ private:
         long ans = 0;
         string vowel = "aeiou";
 
-        if (dp[n][map[prev]] != -1)
-            return dp[n][map[prev]];
-        for (char &c : vowel)
+        if (dp[n][prev] != -1)
+            return dp[n][prev];
+
+        for (int p : mappings[prev])
         {
-            if (c == 'a' and (prev == ' ' or prev == 'e' or prev == 'u' or prev == 'i'))
-                ans = (ans + count_vovel_per(n - 1, dp, 'a') % mod) % mod;
-
-            if (c == 'e' and (prev == ' ' or prev == 'a' or prev == 'i'))
-                ans = (ans + count_vovel_per(n - 1, dp, 'e') % mod) % mod;
-
-            if (c == 'i' and (prev == ' ' or prev == 'e' or prev == 'o'))
-                ans = (ans + count_vovel_per(n - 1, dp, 'i') % mod) % mod;
-
-            if (c == 'o' and (prev == ' ' or prev == 'i'))
-                ans = (ans + count_vovel_per(n - 1, dp, 'o') % mod) % mod;
-
-            if (c == 'u' and (prev == ' ' or prev == 'o' or prev == 'i'))
-                ans = (ans + count_vovel_per(n - 1, dp, 'u') % mod) % mod;
+            ans = (ans + count_vovel_per(n - 1, dp, p) % mod) % mod;
         }
+        // for (char &c : vowel)
+        // {
+        //     if (c == 'a' and (prev == ' ' or prev == 'e' or prev == 'u' or prev == 'i'))
+        //         ans = (ans + count_vovel_per(n - 1, dp, 'a') % mod) % mod;
 
-        return dp[n][map[prev]] = ans;
+        //     if (c == 'e' and (prev == ' ' or prev == 'a' or prev == 'i'))
+        //         ans = (ans + count_vovel_per(n - 1, dp, 'e') % mod) % mod;
+
+        //     if (c == 'i' and (prev == ' ' or prev == 'e' or prev == 'o'))
+        //         ans = (ans + count_vovel_per(n - 1, dp, 'i') % mod) % mod;
+
+        //     if (c == 'o' and (prev == ' ' or prev == 'i'))
+        //         ans = (ans + count_vovel_per(n - 1, dp, 'o') % mod) % mod;
+
+        //     if (c == 'u' and (prev == ' ' or prev == 'o' or prev == 'i'))
+        //         ans = (ans + count_vovel_per(n - 1, dp, 'u') % mod) % mod;
+        // }
+
+        return dp[n][prev] = ans;
         // return ans;
     }
 
 public:
     int countVowelPermutation(int n)
     {
-        map['a'] = 0;
-        map['e'] = 1;
-        map['i'] = 2;
-        map['o'] = 3;
-        map['u'] = 4;
 
-        vector<vector<long>> dp(n + 1, vector<long>(5, -1));
+        vector<vector<long>> dp(n + 1, vector<long>(6, -1));
 
         return count_vovel_per(n, dp);
     }
