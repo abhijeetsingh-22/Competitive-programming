1889269aa991da92c3ac656507fc247046a36401
Abhijeet Singh <asingh9829@gmail.com>
2022-08-08 02:01:08 +0530

    7/8/22 -3 memoization

--- Leetcode/63.unique-paths-ii.cpp
+++ Leetcode/63.unique-paths-ii.cpp
@@ -7,7 +7,7 @@
 // @lc code=start
 class Solution
 {
-    static int ways_to_reach(int row, int col, vector<vector<int>> &grid)
+    static int ways_to_reach(int row, int col, vector<vector<int>> &grid, vector<vector<int>> &dp)
     {
         if (row < 0 or col < 0)
             return 0;
@@ -15,11 +15,12 @@ class Solution
             return 0;
         if (row == 0 and col == 0)
             return 1;
-
+        if (dp[row][col] != -1)
+            return dp[row][col];
         // recursive case;
-        int up = ways_to_reach(row - 1, col, grid);
-        int left = ways_to_reach(row, col - 1, grid);
-        return up + left;
+        int up = ways_to_reach(row - 1, col, grid, dp);
+        int left = ways_to_reach(row, col - 1, grid, dp);
+        return dp[row][col] = up + left;
     }
 
 public:
@@ -27,8 +28,8 @@ public:
     {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-
-        return ways_to_reach(m - 1, n - 1, obstacleGrid);
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return ways_to_reach(m - 1, n - 1, obstacleGrid, dp);
     }
 };
 // @lc code=end
