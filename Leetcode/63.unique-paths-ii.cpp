3351bcf6c824bd832fd0b441d23a054e24f5f13c
Abhijeet Singh <asingh9829@gmail.com>
2022-08-08 02:08:05 +0530

    7/8/22 -3 bottom up

--- Leetcode/63.unique-paths-ii.cpp
+++ Leetcode/63.unique-paths-ii.cpp
@@ -22,14 +22,45 @@ class Solution
         int left = ways_to_reach(row, col - 1, grid, dp);
         return dp[row][col] = up + left;
     }
+    static int ways_bottomup(int m, int n, vector<vector<int>> &grid)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    dp[i][j] = 0;
+                }
+
+                else if (i == 0 and j == 0)
+                    dp[i][j] = 1;
+                else
+                {
+
+                    int up = 0, left = 0;
+                    if (i > 0)
+                        up = dp[i - 1][j];
+                    if (j > 0)
+                        left = dp[i][j - 1];
+                    dp[i][j] = left + up;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
 
 public:
     int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
     {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return ways_to_reach(m - 1, n - 1, obstacleGrid, dp);
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return ways_to_reach(m - 1, n - 1, obstacleGrid, dp);
+        return ways_bottomup(m, n, obstacleGrid);
     }
 };
 // @lc code=end
