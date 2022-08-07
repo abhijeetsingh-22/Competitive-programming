3975bcf96a5f42b44e8b99fc6ba932fef38126ed
Abhijeet Singh <asingh9829@gmail.com>
2022-08-08 02:38:07 +0530

    7/8/22 -4 bottom up dp

--- Leetcode/64.minimum-path-sum.cpp
+++ Leetcode/64.minimum-path-sum.cpp
@@ -22,14 +22,40 @@ class Solution
 
         return dp[m][n] = min(left, up) + grid[m][n];
     }
+    static int min_path_bottomup(vector<vector<int>> &grid)
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        dp[0][0] = grid[0][0];
+
+        for (int row = 0; row < m; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (row == 0 and col == 0)
+                    continue;
+                int up = INT_MAX, left = INT_MAX;
+                if (row > 0)
+                    up = dp[row - 1][col];
+                if (col > 0)
+                    left = dp[row][col - 1];
+
+                dp[row][col] = min(left, up) + grid[row][col];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
 
 public:
     int minPathSum(vector<vector<int>> &grid)
     {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return min_path(m - 1, n - 1, grid, dp);
+        // int m = grid.size();
+        // int n = grid[0].size();
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return min_path(m - 1, n - 1, grid, dp);
+        return min_path_bottomup(grid);
     }
 };
 // @lc code=end
