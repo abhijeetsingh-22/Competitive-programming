36051ce490161c1b2b1d82dd29fa24d5f4a52d06
Abhijeet Singh <asingh9829@gmail.com>
2022-08-07 17:43:11 +0530

    7/8/22 -2

---
+++ Leetcode/931.minimum-falling-path-sum.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=931 lang=cpp
+ *
+ * [931] Minimum Falling Path Sum
+ */
+
+// @lc code=start
+class Solution
+{
+private:
+    static int min_path_sum(int idx, int prev_col, vector<vector<int>> &matrix, vector<vector<int>> &dp)
+    {
+        if (idx < 0)
+            return 0;
+
+        if (dp[idx][prev_col] != -1)
+            return dp[idx][prev_col];
+
+        int n = matrix[0].size();
+        int ans = INT_MAX;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == prev_col - 1 or i == prev_col - 2 or i == prev_col or prev_col == 0)
+                ans = min(ans, min_path_sum(idx - 1, i + 1, matrix, dp) + matrix[idx][i]);
+        }
+        return dp[idx][prev_col] = ans;
+    }
+
+public:
+    int minFallingPathSum(vector<vector<int>> &matrix)
+    {
+        int n = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        return min_path_sum(n - 1, 0, matrix, dp);
+    }
+};
+// @lc code=end
