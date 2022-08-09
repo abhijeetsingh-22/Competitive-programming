bfb3c32f21c6c5322aa30f6511e1422daba74d77
Abhijeet Singh <asingh9829@gmail.com>
2022-08-10 03:55:27 +0530

    9/8/22 -3 space optimization

--- Leetcode/416.partition-equal-subset-sum.cpp
+++ Leetcode/416.partition-equal-subset-sum.cpp
@@ -59,6 +59,39 @@ class Solution
         }
         return dp[n - 1][sum];
     }
+    bool can_partition_sopt(vector<int> &nums)
+    {
+        int n = nums.size();
+        int sum = 0;
+        for (int &x : nums)
+        {
+            sum += x;
+        }
+        if (sum & 1)
+            return false;
+
+        vector<bool> prev(sum + 1, false);
+        sum = sum / 2;
+        prev[0] = true;
+        prev[nums[0]] = true;
+
+        for (int idx = 1; idx < n; idx++)
+        {
+            vector<bool> cur(sum + 1, false);
+            cur[0] = true;
+            for (int target = 1; target <= sum; target++)
+            {
+                bool not_take = prev[target];
+                bool take = false;
+                if (target >= nums[idx])
+                    take = prev[target - nums[idx]];
+                // cout << idx << "   " << target << "  " << take << "  " << not_take << endl;
+                cur[target] = take or not_take;
+            }
+            prev = cur;
+        }
+        return prev[sum];
+    }
 
 public:
     bool canPartition(vector<int> &nums)
@@ -73,7 +106,8 @@ public:
         // }
         // vector<vector<int>> dp(n, vector<int>(sum / 2 + 1, -1));
         // return sum & 1 ? false : can_partition(n - 1, sum / 2, nums, dp);
-        return can_partition_bottomup(nums);
+        // return can_partition_bottomup(nums);
+        return can_partition_sopt(nums);
     }
 };
 // @lc code=end
