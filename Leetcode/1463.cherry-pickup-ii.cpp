185bdd2677537c86751d6f5e1f1d2a415ac7a843
Abhijeet Singh <asingh9829@gmail.com>
2022-08-10 00:15:59 +0530

    9/8/22 -2 space optimization

--- Leetcode/1463.cherry-pickup-ii.cpp
+++ Leetcode/1463.cherry-pickup-ii.cpp
@@ -76,6 +76,47 @@ class Solution
         }
         return dp[0][0][c - 1];
     }
+    int max_cherry_sopt(vector<vector<int>> &grid)
+    {
+
+        int r = grid.size();
+        int c = grid[0].size();
+        vector<vector<int>> front(c, vector<int>(c, 0));
+
+        for (int col1 = 0; col1 < c; col1++)
+            for (int col2 = 0; col2 < c; col2++)
+            {
+                if (col1 == col2)
+                    front[col1][col2] = grid[r - 1][col1];
+                else
+                    front[col1][col2] = grid[r - 1][col1] + grid[r - 1][col2];
+            }
+        for (int row = r - 2; row >= 0; row--)
+        {
+            vector<vector<int>> cur(c, vector<int>(c, 0));
+
+            for (int col1 = c - 1; col1 >= 0; col1--)
+                for (int col2 = c - 1; col2 >= 0; col2--)
+                {
+                    int value = 0;
+                    for (int dc1 = -1; dc1 <= 1; dc1++)
+                        for (int dc2 = -1; dc2 <= 1; dc2++)
+                        {
+                            if (col1 + dc1 < 0 or col1 + dc1 >= c or col2 + dc2 < 0 or col2 + dc2 >= c)
+                                continue;
+                            value = max(value, front[col1 + dc1][col2 + dc2]);
+                        }
+                    if (col1 == col2)
+                        value += grid[row][col1];
+                    else
+                        value += grid[row][col1] + grid[row][col2];
+                    cur[col1][col2] = value;
+                }
+
+            front = cur;
+        }
+        return front[0][c - 1];
+    }
 
 public:
     int cherryPickup(vector<vector<int>> &grid)
@@ -85,7 +126,8 @@ public:
         // vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c, -1)));
 
         // return max_cherry(0, 0, c - 1, r, c, grid, dp);
-        return max_cherry_bottomup(grid);
+        // return max_cherry_bottomup(grid);
+        return max_cherry_sopt(grid);
     }
 };
 // @lc code=end
