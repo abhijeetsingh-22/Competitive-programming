2ff3031207658c1e0d2a9f95ebfd59f0a74bd89d
Abhijeet Singh <asingh9829@gmail.com>
2022-08-11 18:15:33 +0530

    11/8/22 -2 morris

--- Leetcode/98.validate-binary-search-tree.cpp
+++ Leetcode/98.validate-binary-search-tree.cpp
@@ -28,15 +28,59 @@ class Solution
 
         return is_valid_bst(root->left, lb, root->val) and is_valid_bst(root->right, root->val, ub);
     }
+    bool is_valid_bst_morris(TreeNode *root)
+    {
+        if (root == nullptr)
+            return true;
+        TreeNode *cur = root;
+        long long last_number = INT_MIN;
+        last_number--;
+        bool ans = true;
+        while (cur != nullptr)
+        {
+            if (cur->left)
+            {
+                TreeNode *prev = cur->left;
+                while (prev->right != nullptr and prev->right != cur)
+                {
+                    prev = prev->right;
+                }
+                if (prev->right == nullptr)
+                {
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                if (prev->right == cur)
+                {
+                    prev->right = nullptr;
+                    if (cur->val <= last_number)
+                        ans = false;
+                    // last_number = 5;
+                    last_number = (long long)cur->val;
+                    cur = cur->right;
+                }
+            }
+            else
+            {
+
+                if (cur->val <= last_number)
+                    ans = false;
+                last_number = cur->val;
+                cur = cur->right;
+            }
+        }
+        return ans;
+    }
 
 public:
     bool isValidBST(TreeNode *root)
     {
-        long long lb = INT_MIN;
-        lb--;
-        long long ub = INT_MAX;
-        ub++;
-        return is_valid_bst(root, lb, ub);
+        // long long lb = INT_MIN;
+        // lb--;
+        // long long ub = INT_MAX;
+        // ub++;
+        // return is_valid_bst(root, lb, ub);
+        return is_valid_bst_morris(root);
     }
 };
 // @lc code=end
