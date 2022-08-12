cd637d4a875951de14112b8bcd91d73cf75c7337
Abhijeet Singh <asingh9829@gmail.com>
2022-08-12 22:21:36 +0530

    11/8/22 -3 space optimization

--- Leetcode/518.coin-change-2.cpp
+++ Leetcode/518.coin-change-2.cpp
@@ -24,14 +24,64 @@ class Solution
 
         return dp[idx][target] = take + not_take;
     }
+    int count_ways_bottomup(vector<int> &coins, int amount)
+    {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+
+        for (int target = 0; target <= amount; target++)
+        {
+            if (target == 0 or target % coins[0] == 0)
+                dp[0][target] = 1;
+        }
+        for (int idx = 1; idx < n; idx++)
+        {
+            for (int target = 0; target <= amount; target++)
+            {
+                int not_take = dp[idx - 1][target];
+                int take = 0;
+                if (target >= coins[idx])
+                    take = dp[idx][target - coins[idx]];
+
+                dp[idx][target] = take + not_take;
+            }
+        }
+        return dp[n - 1][amount];
+    }
+    int count_ways_sopt(vector<int> &coins, int amount)
+    {
+        int n = coins.size();
+        vector<int> prev(amount + 1, 0), cur(amount + 1, 0);
+
+        for (int target = 0; target <= amount; target++)
+        {
+            if (target == 0 or target % coins[0] == 0)
+                prev[target] = 1;
+        }
+        for (int idx = 1; idx < n; idx++)
+        {
+            for (int target = 0; target <= amount; target++)
+            {
+                int not_take = prev[target];
+                int take = 0;
+                if (target >= coins[idx])
+                    take = cur[target - coins[idx]];
+                cur[target] = take + not_take;
+            }
+            prev = cur;
+        }
+        return prev[amount];
+    }
 
 public:
     int change(int amount, vector<int> &coins)
     {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        // int n = coins.size();
+        // vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
 
-        return count_ways(n - 1, amount, coins, dp);
+        // return count_ways(n - 1, amount, coins, dp);
+        // return count_ways_bottomup(coins, amount);
+        return count_ways_sopt(coins, amount);
     }
 };
 // @lc code=end
