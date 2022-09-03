44adf2c781fcd9a880f37ed2ff606388da671c2a
Abhijeet Singh <asingh9829@gmail.com>
2022-09-03 17:50:14 +0530

    3/9/22 -2

---
+++ Leetcode/1671.minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=1671 lang=cpp
+ *
+ * [1671] Minimum Number of Removals to Make Mountain Array
+ */
+
+ // @lc code=start
+class Solution {
+    int max_bitonic(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp1(n, 1), dp2(n, 1);
+
+        for (int idx = 0;idx < n;idx++) {
+            for (int prev_idx = 0;prev_idx < idx;prev_idx++) {
+                if (nums[idx] > nums[prev_idx] and dp1[idx] < dp1[prev_idx] + 1) {
+                    dp1[idx] = 1 + dp1[prev_idx];
+                }
+            }
+        }
+        for (int idx = n - 1;idx >= 0;idx--) {
+            for (int prev_idx = n - 1;prev_idx > idx;prev_idx--) {
+                if (nums[idx] > nums[prev_idx] and dp2[idx] < dp2[prev_idx] + 1) {
+                    dp2[idx] = 1 + dp2[prev_idx];
+                }
+            }
+        }
+        int maxi = 0;
+
+        cout << endl;
+        for (int i = 0;i < n;i++) {
+            if (dp1[i] >= 2 and dp2[i] >= 2) {
+                maxi = max(maxi, dp1[i] + dp2[i] - 1);
+            }
+        }
+        return maxi;
+    }
+
+public:
+    int minimumMountainRemovals(vector<int>& nums) {
+        int n = nums.size();
+        return n - max_bitonic(nums);
+    }
+};
+// @lc code=end
+
