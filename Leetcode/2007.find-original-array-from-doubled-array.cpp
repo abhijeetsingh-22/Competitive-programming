547d133100373a05d4fad4eb18609657b1194413
Abhijeet Singh <asingh9829@gmail.com>
2022-09-15 20:22:29 +0530

    15/9/22 -1

---
+++ Leetcode/2007.find-original-array-from-doubled-array.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=2007 lang=cpp
+ *
+ * [2007] Find Original Array From Doubled Array
+ */
+
+// @lc code=start
+class Solution {
+   public:
+    vector<int> findOriginalArray(vector<int>& changed) {
+        int n = changed.size();
+        if (n & 1)
+            return {};
+        unordered_map<int, int> map;
+        vector<int> ans;
+        sort(changed.begin(), changed.end());
+        for (int num : changed) {
+            if (!(num & 1) and map[num / 2] > 0) {
+                map[num / 2]--;
+                ans.push_back(num / 2);
+            } else if (map[2 * num] > 0) {
+                map[2 * num]--;
+                ans.push_back(num);
+            } else
+                map[num]++;
+        }
+        for (pair<int, int> p : map) {
+            if (p.second != 0)
+                return {};
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
